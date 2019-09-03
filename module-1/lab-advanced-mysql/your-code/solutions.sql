78e38ceff11356c66710a03e416a08a82bf9fd9d
Luis-Porras <porrasluis247@gmail.com>
2019-09-03 14:08:47 -0400

    finished sql

---
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -0,0 +1,93 @@
+#CHALLENGE 1
+#using derived tables
+
+#Step 1: Calculate the royalties of each sales for each author
+SELECT titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+from authors
+join titleauthor
+on authors.au_id = titleauthor.au_id
+join titles
+on titleauthor.title_id = titles.title_id
+join sales
+on titles.title_id = sales.title_id
+order by sales_royalty desc
+
+# Step 2: Aggregate the total royalties for each title for each author
+Select SUM(sales_royalty) as royalty1, au_id, title_id from (
+	SELECT titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+	from authors
+	join titleauthor
+	on authors.au_id = titleauthor.au_id
+	join titles
+	on titleauthor.title_id = titles.title_id
+	join sales
+	on titles.title_id = sales.title_id
+	order by sales_royalty desc
+	)  as tt 
+group by au_id, title_id;
+
+#Step 3: Calculate the total profits of each author
+Select titles.advance + t2.royalty1 as profits, t2.au_id from (
+	Select SUM(sales_royalty) as royalty1, au_id, title_id from (
+		SELECT titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+		from authors
+		join titleauthor
+		on authors.au_id = titleauthor.au_id
+		join titles
+		on titleauthor.title_id = titles.title_id
+		join sales
+		on titles.title_id = sales.title_id
+		order by sales_royalty desc
+		)  as t1
+	group by au_id, title_id) as t2
+join titles 
+on titles.title_id = t2.title_id
+order by profits desc limit 3;
+
+
+# CHALLENGE 2 - Alternative Solution
+#Using temp tables
+
+#Step 1: Calculate the royalties of each sales for each author
+CREATE TEMPORARY TABLE step1
+SELECT titles.title_id, authors.au_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+		from authors
+		join titleauthor
+		on authors.au_id = titleauthor.au_id
+		join titles
+		on titleauthor.title_id = titles.title_id
+		join sales
+		on titles.title_id = sales.title_id
+		order by sales_royalty desc;
+
+# Step 2: Aggregate the total royalties for each title for each author
+CREATE TEMPORARY TABLE step2
+Select SUM(sales_royalty) as royalty1, au_id, title_id
+from step1
+group by au_id, title_id; 
+
+#Step 3: Calculate the total profits of each author
+Select titles.advance + step2.royalty1 as profits, step2.au_id 
+from step2
+join titles 
+on titles.title_id = step2.title_id
+order by profits desc limit 3;
+
+#CHALLENGE 3 
+CREATE TABLE most_profitting_authors
+Select titles.advance + step2.royalty1 as profits, step2.au_id 
+from step2
+join titles 
+on titles.title_id = step2.title_id
+order by profits desc limit 3;
+
+
+
+
+
+
+
+
+
+
+
--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -7,25 +7,35 @@ Inner JOIN titleauthor ON authors.au_id = titleauthor.au_id
 ORDER BY authors.au_id;
 
 
-
-###1 repeat
-SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, publishers.pub_name
-FROM authors 
-Inner JOIN titleauthor ON authors.au_id = titleauthor.au_id
-	  JOIN titles ON titleauthor.title_id = titles.title_id
-	  JOIN publishers ON titles.pub_id = publishers.pub_id
-ORDER BY authors.au_lname;
-
 #2
-SELECT authors.au_id, authors.au_lname as LastName, authors.au_fname as FirstName, count(titles.title) as TitleCount, publishers.pub_name as Publisher
+SELECT authors.au_id, authors.au_fname as "first", authors.au_lname as "last", count(titles.title) as title_count, publishers.pub_name as publisher
 FROM authors
-JOIN titleauthor
-ON authors.au_id = titleauthor.au_id
-JOIN titles
-ON titles.title_id = titleauthor.title_id
-JOIN publishers
-ON
-titles.pub_id = publishers.pub_id
-GROUP BY authors.au_id, publishers.pub_id
+join titleauthor
+on authors.au_id = `titleauthor`.au_id
+join titles
+on titleauthor.title_id = titles.title_id
+join publishers
+on titles.pub_id = publishers.pub_id
+group by authors.au_id, publishers.pub_name
+Order by authors.au_id desc;
 
+#3
+SELECT authors.au_id as au_id,  authors.au_lname as last, authors.au_fname as first, sum(sales.qty) as total
+from authors
+join titleauthor
+on authors.au_id = titleauthor.au_id
+join sales
+on titleauthor.title_id = sales.title_id
+group by au_id
+order by total desc limit 3;
+
+#4
+SELECT authors.au_id as au_id,  authors.au_lname as last, authors.au_fname as first, ifnull(sum(sales.qty),0) as total
+from authors
+left join titleauthor
+on authors.au_id = titleauthor.au_id
+left join sales
+on titleauthor.title_id = sales.title_id
+group by au_id
+order by total desc;
 
