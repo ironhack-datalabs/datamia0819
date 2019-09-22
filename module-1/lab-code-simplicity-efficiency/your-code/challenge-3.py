d91fe128178e6ea7cb26323a3d57d166668bc405
Luis-Porras <porrasluis247@gmail.com>
2019-09-22 19:55:04 -0400

    week 5

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-3.py
@@ -28,3 +28,15 @@ def my_function(X):
 X = input("What is the maximal length of the triangle side? Enter a number: ")
 
 print("The longest side possible is " + str(my_function(int(X))))
+
+#solution i guess.
+def my_function(X):
+    solutions = [ [x, y, z] for x in range(5, X) for y in range(4, X) for z in range(3, X) if (x*x==y*y+z*z)]
+    return print("Longest side possible is: ",max(max(solutions)))
+
+my_function(int(input("What is the maximal length of the triangle side? Enter a number: ")))
+                
+                
+                
+                
+                
